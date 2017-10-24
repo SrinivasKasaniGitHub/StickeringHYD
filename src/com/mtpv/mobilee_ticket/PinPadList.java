1c734900e1c8c226a6f991af2c5276a2d424d007
Srinivas <srinivaskasani.mtpv@gmail.com>
2017-10-24 16:16:24 +0530

    Stickering initial commit on 25 oct

---
+++ src/com/mtpv/mobilee_ticket/PinPadList.java
@@ -0,0 +1,16 @@
+package com.mtpv.mobilee_ticket;
+
+import com.mtpv.mobilee_stickering.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+@SuppressWarnings("unused")
+public class PinPadList extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pin_pad_list);
+	}
+}
---
+++ src/com/mtpv/mobilee_ticket/SpecialDrive.java
@@ -0,0 +1,15 @@
+package com.mtpv.mobilee_ticket;
+
+import com.mtpv.mobilee_stickering.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpecialDrive extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_special_drive);
+	}
+}
